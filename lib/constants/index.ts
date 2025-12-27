55794dd73dab6530a09eeb085b083d52d9916f70
vtempest <you@example.com>
2025-12-27 03:48:38 -0800

    api docs

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -2,9 +2,10 @@ export * from './category'
 export * from './tags'
 
 export const APP_NAME = process?.env?.NEXT_PUBLIC_APP_NAME;
+export const APP_EMAIL = process?.env?.NEXT_PUBLIC_APP_EMAIL;
 
+export const DOCS_DESCRIPTION = "Investing with research agents, algorithmic strategies, and prediction markets"
 
-export const DOCS_DESCRIPTION = "Investing with research agents, algorithmic strategies, and prediction markets",
-    APP_EMAIL = "support@autoinvestment.broker",
-    LAST_REVISED_DATE = "2025-12-16",
-    DOCS_TITLE = APP_NAME + " Docs";
+export const LAST_REVISED_DATE = "2025-12-16"
+
+export const DOCS_TITLE = APP_NAME + " Docs";
--- lib/customize-site.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Constant variables for site customization
- */
-export const
-    APP_EMAIL = "support@autoinvestment.broker",
-    LAST_REVISED_DATE = "2025-12-16"
